6b4bae1947f6464a2e03916e2caddc1b99890d65
ema.cikotic <emacikotic25@gmail.com>
2026-08-01 15:57:18 +0200

    monthly inscription activity

--- src/main/java/InscriptionReader.java
+++ src/main/java/InscriptionReader.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper; //https://www.baeldung.com/j
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
+import java.time.YearMonth;
+import java.util.Map;
+import java.util.HashMap;
 
 public class InscriptionReader {
     public void readFile() {
@@ -18,10 +21,20 @@ public class InscriptionReader {
             long earliestTimestamp = Long.MAX_VALUE;
             long latestTimestamp = Long.MIN_VALUE;
 
+            Map<YearMonth, Integer> monthlyActivity = new HashMap<>();
+
            while (line != null) {
 
                 Inscription inscription = mapper.readValue(line, Inscription.class);
                 long timestamp=inscription.getTimestamp();
+
+               // Convert timestamp -> LocalDate -> YearMonth
+               LocalDate date = Instant.ofEpochSecond(timestamp).atZone(ZoneOffset.UTC).toLocalDate();
+
+               YearMonth month = YearMonth.from(date);
+
+               monthlyActivity.put(month, monthlyActivity.getOrDefault(month, 0) + 1);
+
 
                if (timestamp < earliestTimestamp)  earliestTimestamp = timestamp;
 
@@ -39,6 +52,12 @@ public class InscriptionReader {
             System.out.println("From: " + earliestDate);
             System.out.println("To: " + latestDate);
             System.out.println("Total inscriptions: " + String.format("%,d", counter));
+
+            System.out.println("\nMonthly activity:");
+
+            for (Map.Entry<YearMonth, Integer> entry : monthlyActivity.entrySet()) {
+                System.out.println(entry.getKey() + " -> " + String.format("%,d", entry.getValue()));
+            }
 
         } catch (IOException e) {
             System.out.println("Error reading file.");
